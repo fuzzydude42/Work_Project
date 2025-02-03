b9c131323134464ae58ba94b46b3b13a469096c3
fuzzydude42 <99512660+fuzzydude42@users.noreply.github.com>
2025-02-03 14:53:17 -0500

    Update backup.sh
    Updated to newest version, in line with update Windows WSL for temp folder location

--- backup.sh
+++ backup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
 #bash creates varibales <scriptname> <$1> <$2> <$3> <...>
+#make sure gawk (GNU Awk) is installed "sudo apt install gawk"
+#link script into /bin to be able to run it anywhere, and changes the name, dropping the suffix
+#sudo ln -s /home/[user]/script/backup.sh /bin/backup
+
+#find tmpfs folders with "df -h"
+
 file=$1
 folder=${file%%.*}  #removes everything after first period from file name
 folder=$2"_"$folder"_backup"
@@ -8,20 +14,19 @@ then
   printf "\nNo file selected \nUsage  backup.sh <filename> <machineName> \n\n"
   exit 1
 fi
-TIMEFORMAT="Backup took %Rs"
-time {
 ranNum=$(($RANDOM % 999999 + 100000))
-mkdir /tmp/$ranNum
-cp $file /tmp/$ranNum/
-pushd /tmp/$ranNum/
+#formaly used /tmp.. but its not tmpfs under wsl
+working=/run/user/1002/
+mkdir "$working"$ranNum
+cp $file "$working"$ranNum/
+pushd "$working"$ranNum/
 cat $file | tr -d '\r%<>' > tr1  #removes \r(return), %, <, >, from file
 sed 's/^OO/O/' tr1 > allprog #if line starts OO replace with O
 awk NF allprog >> 2allprog #remove blank lines
-awk '/^O/' 2allprog > _ATableOfContents.txt #create a table of contents file
-awk '/^$/ {next} BEGIN {FIELDWIDTHS="5"} /^O/ {fn=$1 ".NC";print "%">fn}{print>fn}' 2allprog #create program files from source
-for i in *.NC ; do echo "%" >> $i ; done
+awk '/^O/' 2allprog > ATableOfContents.txt  #create a table of contents with first line of each prog
+awk '/^$/ {next} BEGIN {FIELDWIDTHS="5"} /^O/ {fn=$1 ".NC";print "%">fn}{print>fn}' 2allprog #create program files from>for i in *.NC ; do echo "%" >> $i ; done
 popd
 mkdir $folder$ranNum
-cp /tmp/$ranNum/*.NC ./$folder$ranNum/
-cp /tmp/$ranNum/_ATableOfContents.txt ./$folder$ranNum/   #copy it back to source folder
-}
+cp "$working"$ranNum/*.NC ./$folder$ranNum/
+cp "$working"$ranNum/ATableOfContents.txt ./$folder$ranNum/
+rm -r "$working"$ranNum
