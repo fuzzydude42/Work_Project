47f9d26f352c6d9b7859b57c5b6658b165e4fde1
fuzzydude42 <99512660+fuzzydude42@users.noreply.github.com>
2025-02-11 17:31:19 -0500

    Update backup.sh
    -Changed to date/time folder naming scheme from random number for a more logical order. 
    -Added a 2 second delay at the end of the script to avoid collisions if the script is run repeatedly.

--- backup.sh
+++ backup.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/bash
 #bash creates varibales <scriptname> <$1> <$2> <$3> <...>
-#make sure gawk (GNU Awk) is installed "sudo apt install gawk"
-#link script into /bin to be able to run it anywhere, and changes the name, dropping the suffix
-#sudo ln -s /home/[user]/script/backup.sh /bin/backup
-
-#find tmpfs folders with "df -h"
-
 file=$1
 folder=${file%%.*}  #removes everything after first period from file name
 folder=$2"_"$folder"_backup"
@@ -14,19 +8,19 @@ then
   printf "\nNo file selected \nUsage  backup.sh <filename> <machineName> \n\n"
   exit 1
 fi
-ranNum=$(($RANDOM % 999999 + 100000))
-#formaly used /tmp.. but its not tmpfs under wsl
+now="_$(date +%m-%d-%Y_%H-%M-%S)" #adjust for local date/time format
 working=/run/user/1002/
-mkdir "$working"$ranNum
-cp $file "$working"$ranNum/
-pushd "$working"$ranNum/
+mkdir "$working"$now
+cp $file "$working"$now/
+pushd "$working"$now/
 cat $file | tr -d '\r%<>' > tr1  #removes \r(return), %, <, >, from file
 sed 's/^OO/O/' tr1 > allprog #if line starts OO replace with O
 awk NF allprog >> 2allprog #remove blank lines
 awk '/^O/' 2allprog > ATableOfContents.txt  #create a table of contents with first line of each prog
-awk '/^$/ {next} BEGIN {FIELDWIDTHS="5"} /^O/ {fn=$1 ".NC";print "%">fn}{print>fn}' 2allprog #create program files from>for i in *.NC ; do echo "%" >> $i ; done
+awk '/^$/ {next} BEGIN {FIELDWIDTHS="5"} /^O/ {fn=$1 ".NC";print "%">fn}{print>fn}' 2allprog #create program files from sour>for i in *.NC ; do echo "%" >> $i ; done
 popd
-mkdir $folder$ranNum
-cp "$working"$ranNum/*.NC ./$folder$ranNum/
-cp "$working"$ranNum/ATableOfContents.txt ./$folder$ranNum/
-rm -r "$working"$ranNum
+mkdir $folder$now
+cp "$working"$now/*.NC ./$folder$now/
+cp "$working"$now/ATableOfContents.txt ./$folder$now/
+rm -r "$working"$now
+sleep 2
